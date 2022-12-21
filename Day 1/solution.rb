f6fd76a99faba2d3cf4d8d2eea72200dfb92e759
Aurel Ioan Patrutiu <aurelioan93@gmail.com>
2022-12-21 23:59:25 +0100

    Restart Day 1

--- Day 1/solution.rb
+++ Day 1/solution.rb
@@ -9,4 +9,8 @@ File.open('data.txt').readlines.each do |line|
   elf_energy = 0
 end
 
-puts "Elf maximum energy is: #{elfs_data.max} "
+# Part 1
+puts "Elf maximum energy is: #{elfs_data.max}"
+
+# Part 2
+puts "Top 3 elfs energy: #{elfs_data.sort.last(3).sum}"
